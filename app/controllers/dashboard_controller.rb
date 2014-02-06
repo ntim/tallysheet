654e4ae019a5f8e3e1c06b4a46a3394db1092852
Tim Niggemann <niggemann@physik.rwth-aachen.de>
2014-02-06 17:42:48 +0100

    Sortable columns.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,18 @@
 class DashboardController < ApplicationController
+  helper_method :sort_column, :sort_direction
   
   def index
-    @consumers = Consumer.includes(:tallysheet_entries).all()
+    @consumers = Consumer.order(sort_column + " " + sort_direction).all()
+  end
+
+  private
+
+  def sort_column
+    params[:sort] != nil ? params[:sort] : "id"
   end
   
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,19 +3,26 @@ module ApplicationHelper
     if value <= 0
       ('<span style="color:green">%.2f &euro;</span>' % value).html_safe
     else
-      ('<span style="color:red">%.2f &euro;</span>' % value).html_safe
-    end 
+    ('<span style="color:red">%.2f &euro;</span>' % value).html_safe
+    end
   end
-  
+
   def style_credit value
     if value >= 0
       ('<span style="color:green">%.2f &euro;</span>' % value).html_safe
     else
-      ('<span style="color:red">%.2f &euro;</span>' % value).html_safe
-    end 
+    ('<span style="color:red">%.2f &euro;</span>' % value).html_safe
+    end
   end
-  
+
   def numeric?(object)
     true if Float(object) rescue false
+  end
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
 end
