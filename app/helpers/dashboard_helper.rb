305d90bf602287dbb5674baa13fea2aacfd900c4
Tim Niggemann <niggemann@physik.rwth-aachen.de>
2014-02-07 16:36:12 +0100

    Summary line

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,29 @@
 module DashboardHelper
+  def total_debt
+    consumers = Consumer.all
+    total = 0
+    consumers.each do |c|
+      total += c.debt
+    end
+    total
+  end
+  
+  def total_amount_of_beverages
+    consumers = Consumer.all
+    total = 0
+    consumers.each do |c|
+      total += c.amount_of_beverages
+    end
+    total
+  end
+  
+  def total_amount_of_payed_beverages
+    consumers = Consumer.all
+    total = 0
+    consumers.each do |c|
+      total += c.amount_of_payed_beverages
+    end
+    total
+  end
+    
 end
