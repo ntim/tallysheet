44cf1412a795d7f4b6f37a2654a08300a2ce289f
Tim Niggemann <niggemann@physik.rwth-aachen.de>
2014-02-07 10:37:20 +0100

    Sortable using class methods. Styling of checkboxes.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,18 +1,32 @@
 class DashboardController < ApplicationController
-  helper_method :sort_column, :sort_direction
-  
+  helper_method :sort_column, :sort_direction, :sort_numeric_column
   def index
-    @consumers = Consumer.order(sort_column + " " + sort_direction).all()
+    @consumers = Consumer.includes(:tallysheet_entries).order(sort_column + " " + sort_direction).all()
+    if sort_numeric_column != nil
+      method_name = sort_numeric_column
+      dir = sort_direction_numeric
+      if Consumer.respond_to? method_name
+        @consumers.sort_by{|e| dir * e.send(method_name)}
+      end
+    end
   end
 
   private
 
   def sort_column
-    params[:sort] != nil ? params[:sort] : "id"
+    Consumer.column_names.include?(params[:sort]) ? params[:sort] : "id"
   end
-  
+
+  def sort_numeric_column
+    params[:sort_numeric] != nil && Consumer.new.respond_to?(params[:sort_numeric]) ? params[:sort_numeric] : nil
+  end
+
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def sort_direction_numeric
+    params[:direction] == "asc" ? 1 : -1
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,8 +21,15 @@ module ApplicationHelper
 
   def sortable(column, title = nil)
     title ||= column.titleize
-    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    css_class = column == sort_column ? "sortable current #{sort_direction}" : "sortable"
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
+
+  def sortable_numeric(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_numeric_column ? "sortable current #{sort_direction}" : "sortable"
+    direction = column == sort_numeric_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort_numeric => column, :direction => direction}, {:class => css_class}
   end
 end
