483a5fd747d175187a2ca0ea2aaeb2d35aeb0c61
Tim Niggemann <niggemann@physik.rwth-aachen.de>
2014-02-11 13:56:14 +0100

    Url in emails, styling, rendering time.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,29 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery :with => :exception
+  helper_method :rendering_time
+  before_filter :set_rendering_start_time, :set_default_url_options_host
   
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
       username == "tally" && password == "sheet!"
+    end
+  end
+
+  def rendering_time
+    millis = (Time.now.usec - @rendering_start_time).abs / 1000.0
+    "Rendered in %d ms" % millis
+  end
+
+  private
+
+  def set_rendering_start_time
+    @rendering_start_time = Time.now.usec
+  end
+
+  def set_default_url_options_host
+    if request != nil
+      Rails.application.routes.default_url_options[:host] = request.host_with_port
     end
   end
 end
