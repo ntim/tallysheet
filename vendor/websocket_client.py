48d71a864db176a37e245ca18da7b04a56fa4d8e
ntim <niggemann.tim@googlemail.com>
2016-08-19 11:57:16 +0200

    Disable printing, route to localhost development instance.

--- vendor/websocket_client.py
+++ vendor/websocket_client.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 import websocket
-import thread
+import subprocess
 import time
 
 
 def on_message(ws, message):
     print message
+    # Create print command
+    # subprocess.call("echo \"" + message + "\" | lp", shell=True)
 
 
 def on_error(ws, error):
@@ -17,4 +19,7 @@ if __name__ == "__main__":
     ws = websocket.WebSocketApp("ws://localhost:3000/websocket",
                                 on_message=on_message,
                                 on_error=on_error)
-    ws.run_forever()
+    while True:
+        ws.run_forever()
+        # If connection is lost, we land here...
+        time.sleep(1)
